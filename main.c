4ae19e5eebe2e65e2aa36c4412b66fa9f7ab2585
Bokyeong Gwon <ma80424@gmail.com>
2026-03-24 21:59:53 +0900

    master commit

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void master_function(void);
+
 // 인사말을 출력하는 함수
 void print_greeting(void) {
     printf("Hello, Git and GitHub!\n");
@@ -27,6 +29,14 @@ int main(void) {
     
     int min = minus_numbers(num1, num2);
     printf("%d - %d = %d\n", num1, num2, min);
+
+    master_function();
     
     return 0;
+}
+
+
+
+void master_function(void) {
+    printf("This is the master branch function.\n");
 }
